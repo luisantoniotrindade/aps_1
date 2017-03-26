23864bcd77524347acde4759a7c2efebe211e9ad
Luis Trindade <luisantoniotrindade@gmail.com>
2017-03-26 11:31:04 -0300

    adicionando cabecalho

--- src/aps_1/Teste.java
+++ src/aps_1/Teste.java
@@ -3,7 +3,18 @@ package aps_1;
 public class Teste {
 
 	public static void main(String[] args) {
-		System.out.println("Olá Mundo");
+		/*
+		 * Implementar um programa, que lê um arquivo contendo a descrição de uma matriz contendo apenas os valores 1 e 0,
+		 * e imprimir, na tela, a matriz contida no arquivo.
+		 * 
+		 * Com a matriz carregada em memória, criar duas cópias da matriz, e, em uma delas, trocar os valores 1 para 2,
+		 * e na outra, trocar os valores 0 para 2.
+		 * 
+		 * O arquivo que deve ser lido contém, na primeira linha, o número de colunas e linhas da matriz,
+		 * nas linhas seguintes, onde cada uma representa uma linha da matriz, encontram-se os dados de cada coluna daquela linha,
+		 * com uma string formada por 1 e 0.
+		 * 
+		 * */
 		
 	}
 
